9c4107d18f11693cb1978a29e31c1d91e019ad4b
Maxwel _Odhiambo <himaxwell36@gmail.com>
2025-02-03 11:34:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Close menu when nav link is clicked
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+/* Initializing Swiper */
+let swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  /* Responsive breakpoints */
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+$(document).ready(function() {
+	$('.sign-up-form').on('submit', submit);
+	$('.reset-button').on('click', reset);
+});
+
+function submit(e) {
+	$('.sign-up-container').addClass('submitted');
+	e.preventDefault();
+}
+
+function reset() {
+	$('.sign-up-container').removeClass('submitted');
+	$('.sign-up-form')[0].reset();
+}
+
+
+
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  }
+}
+
+
